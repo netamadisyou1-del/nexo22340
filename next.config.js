f0d64b5357f6661c68953401199447f1ccda0f29
netamadisyou1-del <netamadisyou1@gmail.com>
2026-06-11 08:53:36 -0600

    next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['jxjtiiyohksrpfojkckz.supabase.co'],
+  },
+}
+
+module.exports = nextConfig
